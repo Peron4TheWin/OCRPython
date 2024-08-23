998b36af01067d4e7e3b79df1d9bf3a7b731ab04
Peron4TheWin <frandonna12@gmail.com>
2024-08-23 10:22:54 -0300

    start

--- app.py
+++ app.py
@@ -7,7 +7,18 @@ app = Flask(__name__)
 def test():
     file = request.files['image']
     file.save(file.filename)
+    return ImageToText(file.filename)
 
+def ImageToText(img):
+    try:
+        result = ocr.ocr(img, cls=True)
+        string = []
+        for line in result:
+            for x in line:
+                string.append(x[-1][0])
+        return string
+    except:
+        return ["none"]
 
 if __name__ == '__main__':
     app.run()
